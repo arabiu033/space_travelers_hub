683719bc8845cd2e27330e83e188d2c84f65e50d
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-15 00:39:55 +0100

    Add basic structure for mission setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import './assets/styles/styles.css';
 
--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
-const Missions = () => <div />;
+const Missions = () => (
+  <Table striped bordered hover>
+    <thead>
+      <tr>
+        <th>Missions</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>Join/Leave</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Larry the Bird</td>
+        <td>@twitter</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </Table>
+);
 
 export default Missions;
