3f91e77320cf0770392827a099395ecd69cba674
Aminu Rabiu <arabiu033@gmail.com>
2022-11-16 02:32:42 +0100

    Structure the rocket.jsx file

--- src/pages/Rocket.jsx
+++ src/pages/Rocket.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/Rockets/rockets';
+import styles from '../assets/styles/rocket.module.css';
 
-const Rockets = () => <div />;
+let fetch = true;
+const Rockets = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (fetch) {
+      dispatch(fetchRockets());
+      fetch = false;
+    }
+  }, [dispatch]);
+
+  const { rockets } = useSelector((state) => ({ ...state.rockets }));
+  return (
+    rockets.map((rocket) => (
+      <div key={rocket.id} className={styles.grid}>
+        <img src={rocket.flickr_images[0]} alt="rocket" />
+        <div>
+          <h3>{rocket.rocket_name}</h3>
+          <p>{rocket.description}</p>
+          <button type="button">Reserve Rocket</button>
+        </div>
+      </div>
+    ))
+  );
+};
 
 export default Rockets;
