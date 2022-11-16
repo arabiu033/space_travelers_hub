296def519b5233f4378a53f2954a3faf733f2a94
Aminu Rabiu <arabiu033@gmail.com>
2022-11-16 02:32:07 +0100

    Modified the configureStore to add a new reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import spaceReducer from './space/spaceSlice';
+import rocketSlice from './Rockets/rockets';
 
 const store = configureStore({
   reducer: {
     space: spaceReducer,
+    rockets: rocketSlice,
   },
 });
 
