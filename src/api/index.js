71a918c4962857d61d138c53f44dea3c450d2591
Aminu Rabiu <arabiu033@gmail.com>
2022-11-16 02:35:09 +0100

    Fix all linters

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,3 @@ const API = axios.create({ baseURL: 'https://api.spacexdata.com/v3/' });
 
 export const getRockets = () => API.get('/rockets');
 export const getMissions = () => API.get('/missions');
-
