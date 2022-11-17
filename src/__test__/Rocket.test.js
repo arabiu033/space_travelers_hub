67f89fe0090c0bbd7b7e3d864545a74a816b2a54
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-17 22:36:32 +0100

    Add rocket testing

---
+++ src/__test__/Rocket.test.js
@@ -0,0 +1,17 @@
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import Rocket from '../pages/Rocket';
+import store from '../redux/configureStore';
+
+describe('Rockets page tests', () => {
+  it('it renders', () => {
+    const rockets = renderer
+      .create(
+        <Provider store={store}>
+          <Rocket />
+        </Provider>,
+      )
+      .toJSON();
+    expect(rockets).toMatchSnapshot();
+  });
+});
